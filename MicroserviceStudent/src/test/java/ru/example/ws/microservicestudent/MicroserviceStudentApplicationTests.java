29db3b5d8cd8c14679c5519b1f998dd4878868e3
DenisPetkov <fini.denif.99@mail.ru>
2022-02-22 22:24:25 +1000

    Добавление необходимых зависимостей

---
+++ MicroserviceStudent/src/test/java/ru/example/ws/microservicestudent/MicroserviceStudentApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.example.ws.microservicestudent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceStudentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
